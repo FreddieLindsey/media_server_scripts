1163770d8832d87f98359070bc868bf594b5249c
FreddieShoreditch <Freddie@iMac.local>
2014-07-02 16:19:39 +0100

    First working version of the ripping script.
    Removed the secure config file property - this may be brought back
    later.
    
    Removed ability to search for the name and year of a disc - this can be
    put back in later, and might require user input.
    
    First running version of the ripping script.

--- rip_discs.sh
+++ rip_discs.sh
@@ -7,13 +7,10 @@
 #--------------------------------------------------------------------------------------------------#
 
 script=$0
-
 cd `dirname $script`
 script_directory=$(pwd -P)
 
-echo $script_directory
 configfile="$script_directory/media_centre_config.conf"
-configfile_secured='/tmp/config_ripdiscs.cfg'
 
 #--------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------#
@@ -26,10 +23,12 @@ get_source ()
 {
 # Print the location of the config file in use
 if [ -e $configfile ]; then
-	echo "Config file found at:			$configfile
+	echo "
+Config file found at:			$configfile
 	";
 else
-	echo "Config file not found, please check the script:		$0
+	echo "
+Config file not found, please check the script:		$0
 	"
 	exit;
 fi
@@ -48,26 +47,26 @@ get_disc_info () {
 	case $1 in
 	"volume_name") 
 		output=$(diskutil info $2 | grep 'Volume Name: ')
-		output=$(echo $output | sed 's/\(Volume Name: \)//')
+		output=$(echo $output | sed 's/^.*\(Volume Name: \)//')
 		output=$(echo $output | sed 's/\( \)//') # To remove all spaces from the start and end of the text
 		volume_name=$output
 	;;
 	"disc_kind")
-		output=$(drutil status | grep 'Type: ' | grep 'Name: ')
+		output=$(drutil status | grep 'Name: ')
 		name_output=$(echo $output | sed 's/\(Type: \)//')
 		name_output=$(echo $name_output | sed 's/\(Name: \).*//')
 		name_output=$(echo $name_output | sed 's/\( \)//') # To remove all spaces from the start and end of the text
 		disc_kind=$name_output
-		mount_output=$(echo $output | sed 's/\(Name: \)//')
+		mount_output=$(echo $output | sed 's/^.*\(Name: \)//')
 		mount_output=$(echo $mount_output | sed 's/\( \)//') # To remove all spaces from the start and end of the text
 		disc_mount=$mount_output
 	;;
-	"title") 
-		$2=$(echo "$2" | sed 's/\(_\)//')
-		title=$(python $get_movie_info "$2" title) 
+	"title")
+		name=$(echo "$2" | sed 's/\(_\)//')
+		title=$(python $get_movie_info "$name" title 2>&1 /dev/null) 
 	;;
 	"year") 
-		year=$(python $get_movie_info "$2" release_date)
+		year=$(python $get_movie_info "$2" release_date 2>&1 /dev/null)
 		year=${year:1:4}
 	;;
 	esac
@@ -75,11 +74,13 @@ get_disc_info () {
 
 # Rips the disc in the drive to the folder name specified in argument 1 inside the set ripping location 
 makemkv_rip () {
-	echo "Starting ripping the disc in the drive to the folder $1 in $ripping_loc...
+	echo "Starting ripping the disc in the drive to the folder $HOME$ripping_loc/$1...
 	"
-	makemkvoutput=$($makemkvcon mkv disc:0 all "$ripping_loc/$1")
-	echo "Finished ripping the disc in the drive to the folder $1 in $ripping_loc...
+	mkdir $HOME$ripping_loc/$1
+	makemkvoutput=$($makemkvcon mkv disc:0 all --minlength=3600 "$HOME$ripping_loc/$1" | tee /dev/tty)
+	echo "Finished ripping the disc in the drive to the folder $HOME$ripping_loc/$1...
 	"
+	drutil eject
 }
 
 # Checks if makemkvcon is already running, and if so cancels the script
@@ -97,15 +98,36 @@ makemkv_running () {
 
 # Gets the name of the disc from Disk Utility
 disk_utility_name () {
-if [ $disc_kind = "BD-ROM" ]; then
-	echo "The disc inserted is a $disc_kind. Disk Utility will now provide the name for the device at $disc_mount.
-	" >&2
-elif [ $disc_kind = "DVD-ROM" ]; then
-	echo "The disc inserted is a $disc_kind. Disk Utility will now provide the name for the device at $disc_mount.
-	" >&2
-else
-	echo "There is no compatible disc inserted, script will now exit." >&2
+
+# See if the disc inserted, if there is one, is a Blu-ray or a DVD and print this to the user.
+if [ -z "${disc_kind}" ]; then
 	exit
+else
+	if [ $disc_kind = "BD-ROM" ]; then
+		echo "The disc inserted is a $disc_kind. Disk Utility will now provide the name for the device at $disc_mount.
+		" >&2
+	elif [ $disc_kind = "DVD-ROM" ]; then
+		echo "The disc inserted is a $disc_kind. Disk Utility will now provide the name for the device at $disc_mount.
+		" >&2
+	else
+		echo "There is no compatible disc inserted, script will now exit." >&2
+		exit
+	fi
+fi
+
+# Get the name of the disc as printed by the OS
+echo "Reading disc name...
+" >&2
+get_disc_info "volume_name" "$disc_mount"
+echo "Disc name is $volume_name
+" >&2
+}
+
+# Checks the name of the disc, and if blank, replaces it with the volume name
+check_name () {
+if [ "$1" = "()" ]; then
+echo "Received empty name. Will now correct with original volume name" >&2
+disc_name_year=$2
 fi
 }
 
@@ -121,12 +143,8 @@ get_source
 # Check if MakeMKV is already running and if so cancel script
 makemkv_running
 
-# Get the kind  and name of the disc currently inserted, exiting if this fails.
-(get_disc_info "disc_kind" && disk_utility_name) || echo "There is no compatible disc inserted, script will now exit." >&2 && exit
+# Get the kind and name of the disc currently inserted.
+get_disc_info "disc_kind"
+disk_utility_name
 
-name_from_mount
-
-disc_name_year="$(get_disc_info title "$disc_mount") $(get_disc_info release_date "$disc_mount")"
-echo $disc_name_year
-
-makemkv_rip "$disc_name_year"
+makemkv_rip "$volume_name"

78c47bda5d0e09505224ddcb7e49a082d1a2cd48
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2014-08-08 14:56:51 +0100

    Clean redundant files

--- install_filebot_ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $HOME
-
-wget -O filebot.deb http://sourceforge.net/projects/filebot/files/filebot/HEAD/FileBot_4.1.1/filebot_4.1_amd64.deb/download
-
-sudo dpkg -i filebot.deb
-
-rm filebot.deb

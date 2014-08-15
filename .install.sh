8d5f64c05e5d83cb5fb623bbfa1d69dbc28fc308
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2014-08-15 17:43:07 +0100

    Make install more informative/clean install

--- .install.sh
+++ .install.sh
@@ -56,30 +56,36 @@ installer_script () {
 	homebrew)
 		if [[ $homebrew ]]; then
 		echo "
-		Homebrew is installing..." >&2
+Homebrew is installing..." >&2
+		echo "
+		Homebrew is downloading..." >&2
 		curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install | grep -v "wait_for_user if STDIN.tty?" >homebrew.rb
+		echo "
+		Homebrew is installing..." >&2
 		sudo su $username_current -c 'ruby homebrew.rb' >/dev/null 2>&1
 		rm homebrew.rb
 		echo "
-		Homebrew has been successfully installed." >&2
-		fi
-	;;
-	wget)
-		if [[ $wget ]]; then
-		echo "
-		wget is installing..." >&2
+		Required utility (wget) installing..." >&2
 		sudo su $username_current -c 'brew update && brew install wget' >/dev/null 2>&1
 		echo "
-		wget has been successfully installed." >&2
+		Required utility (wget) installed..." >&2
+		echo "
+Homebrew has been successfully installed." >&2
 		fi
 	;;
 	jre)
 		if [[ $java ]]; then
 		echo "
-		Java Runtime Environment is installing..." >&2
+Java Runtime Environment is installing..." >&2
+		echo "
+		Mounting disk image..." >&2
 		java_image_location=$(hdiutil attach "$script_directory/.software/runtime_environment.dmg" | grep /Volumes/ | awk -F $'\t' '{print $NF}')
 		java_image_raw=$(hdiutil attach "$script_directory/.software/runtime_environment.dmg" | grep /Volumes/ | awk -F $'\t' '{print $1}')
+		echo "
+		Installing Java Runtime Environment..." >&2
 		installer -pkg "$java_image_location"/*.pkg -target /
+		echo "
+		Unmounting disk image..." >&2
 		diskutil unmountDisk $java_image_raw >/dev/null 2>&1
 		echo "
 		Java Runtime Environment has been successfully installed." >&2
@@ -88,45 +94,44 @@ installer_script () {
 	transmission_daemon_mac)
 		if [[ $transmission_daemon ]]; then
 		echo "
-		transmission-daemon is installing..." >&2
+Transmission is installing..." >&2
 		sudo su $username_current -c 'brew update && brew install transmission' >/dev/null 2>&1
 		echo "
-		transmission-daemon has been installed successfully.
+Transmission has been installed successfully.
 		" >&2
 		fi
 	;;
 	transmission_daemon_linux)
 		if [[ $transmission_daemon ]]; then
 		echo "
-		transmission-daemon is installing..." >&2
+Transmission is installing..." >&2
 		add-apt-repository -y ppa:transmissionbt/ppa >/dev/null 2>&1
 		apt-get update >/dev/null 2>&1
 		apt-get -y install transmission-common transmission-daemon transmission-cli >/dev/null 2>&1
 		apt-get -f install >/dev/null 2>&1
 		echo "
-		transmission-daemon has been installed successfully.
+Transmission has been installed successfully.
 		" >&2
 		fi
 	;;
 	mkvtoolnix_mac)
 		if [[ $mkvtoolnix ]]; then
 		echo "
-		MKVtoolnix is installing..." >&2
-		brew update >/dev/null 2>&1
-		brew install mkvtoolnix >/dev/null 2>&1
+MKVtoolnix is installing..." >&2
+		sudo su $username_current -c 'brew update && brew install mkvtoolnix' >/dev/null 2>&1
 		echo "
-		MKVtoolnix has been installed successfully
+MKVtoolnix has been installed successfully
 		" >&2
 		fi
 	;;
 	mkvtoolnix_linux)
 		if [[ $mkvtoolnix ]]; then
 		echo "
-		MKVtoolnix is installing..." >&2
+MKVtoolnix is installing..." >&2
 		apt-get update >/dev/null 2>&1
 		apt-get -y install mkvtoolnix >/dev/null 2>&1
 		echo "
-		MKVtoolnix has been installed successfully
+MKVtoolnix has been installed successfully
 		" >&2
 		fi
 	;;
@@ -148,33 +153,33 @@ installer_script () {
 	handbrakecli_linux)
 		if [[ $handbrakecli ]]; then
 		echo "
-		HandBrakeCLI is installing..." >&2
+HandBrakeCLI is installing..." >&2
 		apt-get update >/dev/null 2>&1
 		apt-get -y install handbrake-cli >/dev/null 2>&1
 		apt-get -f install >/dev/null 2>&1
 		echo "
-		HandBrakeCLI has been installed successfully.
+HandBrakeCLI has been installed successfully.
 		" >&2
 		fi
 	;;
 	openssh_server)
 		if [[ $openssh ]]; then
 		echo "
-		openssh-server is installing..." >&2
+openssh-server is installing..." >&2
 		apt-get -y install openssh-server >/dev/null 2>&1
 		apt-get -f install >/dev/null 2>&1
 		echo "
-		openssh-server has been installed successfully." >&2
+openssh-server has been installed successfully." >&2
 		fi
 	;;
 	duckdns)
 		if [[ $duckdns ]]; then
 		echo "
-		DuckDNS is installing..." >&2
-		echo "
-Do you have a dynamic address for duckdns.org? [y/n]" >&2
+		Do you have a dynamic address for duckdns.org? [y/n]" >&2
 		read answer
 		if [[ "$(echo $answer | cut -c 1)" == "y" || "$(echo $answer | cut -c 1)" == "Y" ]]; then
+			echo "
+DuckDNS is installing..." >&2
 			if [[ -d "/usr/local/bin/duckdns" ]]; then
 				rm -rf "/usr/local/bin/duckdns"
 				mkdir "/usr/local/bin/duckdns"
@@ -186,13 +191,13 @@ Do you have a dynamic address for duckdns.org? [y/n]" >&2
 			chmod +x /usr/local/bin/duckdns/duck.sh
 			chown -R $username_current /usr/local/bin/duckdns
 			echo "
-What is your dynamic address for duckdns.org? e.g. for someaddress.duckdns.org type someaddress in below and then hit [ENTER]" >&2
+			What is your dynamic address for duckdns.org? e.g. for someaddress.duckdns.org type someaddress in below and then hit [ENTER]" >&2
 			read dynamicaddress
 			echo "
-Visit duckdns.org and sign in with your login. On the home screen to the website, there will now be a token shown.
-The token is a long sequence of letters, numbers, and hyphons. Copy this and paste it below:
+			Visit duckdns.org and sign in with your login. On the home screen to the website, there will now be a token shown.
+			The token is a long sequence of letters, numbers, and hyphons. Copy this and paste it below:
 
-What is your token for duckdns.org?" >&2
+			What is your token for duckdns.org?" >&2
 			read token
 			duckdns_command="echo url=\"https://www.duckdns.org/update?domains=$dynamicaddress&token=$token&ip=\" | curl -k -o /usr/local/bin/duckdns/duck.log -K -"
 			echo $duckdns_command >> "/usr/local/bin/duckdns/duck.sh"
@@ -202,16 +207,16 @@ What is your token for duckdns.org?" >&2
 			fi
 			/usr/local/bin/duckdns/duck.sh >/dev/null 2>&1
 			if [[ "$(cat /usr/local/bin/duckdns/duck.log)" == "OK" ]]; then echo "
-		DuckDNS installed successfully. Please continue." >&2; else echo "
-		DuckDNS has not been installed correctly, please seek advice on installation from https://www.duckdns.org" >&2; fi
-		fi
+DuckDNS installed successfully. Please continue." >&2; else echo "
+DuckDNS has not been installed correctly, please seek advice on installation from https://www.duckdns.org" >&2; fi
+			fi
 		
 		fi
 	;;
 	plexmediaserver_mac)
 		if [[ $pms ]]; then
 		echo "
-		Plex Media Server is installing..." >&2
+Plex Media Server is installing..." >&2
 		plexmediaserver_version=0.9.9.14.531-7eef8c6
 		wget -O PlexMediaServer.zip http://downloads.plexapp.com/plex-media-server/$plexmediaserver_version/PlexMediaServer-$plexmediaserver_version-OSX.zip >/dev/null 2>&1
 		tar -xvf PlexMediaServer.zip >/dev/null 2>&1
@@ -222,9 +227,9 @@ What is your token for duckdns.org?" >&2
 		sudo spctl --enable --label "Plex_Media_Server"
 		ip_address="$(ifconfig | grep "inet" | grep -v "127.0.0.1\|inet6" | head -n 1 | cut -d ' ' -f 2)"
 		echo "
-		Plex Media Server has now been installed. Double-click on it in your Applications folder to start the server.
-		
-		
+Plex Media Server has now been installed. Double-click on it in your Applications folder to start the server.
+
+
 You can now manage your Plex Media Server at the following address:
 
 http://localhost:32400/web/index.html	(from the computer itself)
@@ -237,17 +242,17 @@ http://$ip_address:32400/web/index.html	(from any other computer on the network)
 	plexmediaserver_linux)
 		if [[ $pms ]]; then
 		echo "
-		Plex Media Server is installing..." >&2
-		add-apt-repository -y "deb http://plex.r.worldssl.net/PlexMediaServer/ubuntu-repo lucid main"
+Plex Media Server is installing..." >&2
+		if [[ "$(cat /etc/apt/sources.list | grep "deb http://plex.r.worldssl.net/PlexMediaServer/ubuntu-repo lucid main")" == "" ]]; then add-apt-repository -y "deb http://plex.r.worldssl.net/PlexMediaServer/ubuntu-repo lucid main"; fi
 		# wget -O plex_pub_key.pub http://plexapp.com/plex_pub_key.pub
 		# apt-key add plex_pub_key.pub
 		# rm plex_pub_key.pub
 		apt-get update >/dev/null 2>&1
-		apt-get -y install plexmediaserver --force-yes # Currently can't get key, so must use force yes command
-		apt-get -f install
+		apt-get -y install plexmediaserver --force-yes >/dev/null 2>&1 # Currently can't get key, so must use force yes command
+		apt-get -f install >/dev/null 2>&1
 		ip_address="$(ifconfig | grep "Bcast" | grep "inet" | cut -d ":" -f 2 | cut -d ' ' -f 1)"
 		echo "
-		Plex Media Server has now been installed.
+Plex Media Server has now been installed.
 		
 		
 You can now manage your Plex Media Server at the following address:
@@ -262,17 +267,17 @@ http://$ip_address:32400/web/index.html	(from any other computer on the network)
 	plexhometheater_mac)
 		if [[ $pht ]]; then
 		echo "
+Plex Home Theater is installing..." >&2
+		echo "
 		
 		
-Please understand that running Plex Home Theater on the same machine as the server is NOT recommended. All resources should be given to the server.
+		Please understand that running Plex Home Theater on the same machine as the server is NOT recommended. All resources should be given to the server.
 
-Roku sell extremely cheap Plex clients. Roku's Roku 3 is the best solution at around £70/\$100.
+		Roku sell extremely cheap Plex clients. Roku's Roku 3 is the best solution at around £70/\$100.
 
-Given this, do you still want to install it? [y/n]"
+		Given this, do you still want to install it? [y/n]"
 		read plexht_answer
 		if [[ "$(echo $plexht_answer | cut -c 1)" == "y" || "$(echo $plexht_answer | cut -c 1)" == "Y" ]]; then
-		echo "
-		Plex Home Theater is installing..." >&2
 		plexhometheater_version="1.2.1.314-7cb0133e"
 		wget -O PlexHomeTheater.zip http://downloads.plexapp.com/plex-home-theater/$plexmediaserver_version/PlexHomeTheater-$plexmediaserver_version-macosx-x86_64.zip >/dev/null 2>&1
 		tar -xvf PlexHomeTheater.zip >/dev/null 2>&1
@@ -282,37 +287,37 @@ Given this, do you still want to install it? [y/n]"
 		sudo spctl --add --label "Plex_Home_Theater" /Applications/Plex\ Home\ Theater.app
 		sudo spctl --enable --label "Plex_Home_Theater"
 		echo "
-		Plex Home Theater has now been installed." >&2
+Plex Home Theater has now been installed." >&2
 		fi
 		fi
 	;;
 	plexhometheater_linux)
 		if [[ $pht ]]; then
 		echo "
+Plex Home Theater is installing..." >&2
+		echo "
 		
 		
-Please understand that running Plex Home Theater on the same machine as the server is NOT recommended. All resources should be given to the server.
+		Please understand that running Plex Home Theater on the same machine as the server is NOT recommended. All resources should be given to the server.
 
-Roku sell extremely cheap Plex clients. Roku's Roku 3 is the best solution at around £70/\$100.
+		Roku sell extremely cheap Plex clients. Roku's Roku 3 is the best solution at around £70/\$100.
 
-Given this, do you still want to install it? [y/n]"
+		Given this, do you still want to install it? [y/n]"
 		read plexht_answer
 		if [[ "$(echo $plexht_answer | cut -c 1)" == "y" || "$(echo $plexht_answer | cut -c 1)" == "Y" ]]; then
-		echo "
-		Plex Home Theater is installing..." >&2
 		add-apt-repository -y ppa:plexapp/plexht >/dev/null 2>&1
 		add-apt-repository -y ppa:pulse-eight/libcec >/dev/null 2>&1
 		apt-get update >/dev/null 2>&1
 		apt-get -y install plexhometheater >/dev/null 2>&1
 		echo "
-		Plex Home Theater has now been installed." >&2
+Plex Home Theater has now been installed." >&2
 		fi
 		fi
 	;;
 	filebot_mac)
 		if [[ $filebot ]]; then
 		echo "
-		FileBot is installing..." >&2
+FileBot is installing..." >&2
 		wget -O filebot.html http://www.filebot.net >/dev/null 2>&1
 		filebot_version=$(cat filebot.html | grep ".app" | awk -F 'type=app' '{print $2}' | awk -F '_' '{print $NF}' | awk -F '.app' '{print $1}' | tail -n 1)
 		rm filebot.html
@@ -325,14 +330,14 @@ Given this, do you still want to install it? [y/n]"
 		sudo spctl --add --label "FileBot" /Applications/FileBot.app
 		sudo spctl --enable --label "FileBot"
 		echo "
-		FileBot has been installed successfully.
+FileBot has been installed successfully.
 		" >&2
 		fi
 	;;
 	filebot_linux)
 		if [[ $filebot ]]; then
 		echo "
-		FileBot is installing..." >&2
+FileBot is installing..." >&2
 		wget -O filebot.html http://www.filebot.net >/dev/null 2>&1
 		filebot_version=$(cat filebot.html | grep deb | awk -F 'amd64' '{print $(NF-1)}' | awk -F '_' '{print $(NF-1)}')
 		rm filebot.html
@@ -342,15 +347,17 @@ Given this, do you still want to install it? [y/n]"
 		apt-get -f -y install >/dev/null 2>&1
 		rm filebot.deb >/dev/null 2>&1
 		echo "
-		FileBot has been installed successfully.
+FileBot has been installed successfully.
 		" >&2
 		fi
 	;;
 	makemkv_mac)
 		if [[ $makemkv ]]; then
 		echo "
-		MakeMKV is installing...
+MakeMKV is installing...
 When prompted, please agree to the license agreement." >&2
+		echo "
+		Downloading MakeMKV..." >&2
 		wget -O makemkv.html http://www.makemkv.com/download >/dev/null 2>&1
 		makemkv_version=$(cat makemkv.html | grep 'MakeMKV v' | awk -F 'MakeMKV v' '{print $2}' | cut -d ' ' -f 1 | head -n 1)
 		rm makemkv.html
@@ -363,62 +370,67 @@ When prompted, please agree to the license agreement." >&2
 		diskutil unmountDisk $makemkv_image_raw >/dev/null 2>&1
 		rm makemkv.dmg
 		echo "
-		MakeMKV has been installed successfully.
+MakeMKV has been installed successfully.
 		" >&2
 		fi
 	;;
 	makemkv_linux)
 		if [[ $makemkv ]]; then
 		echo "
-		MakeMKV is installing..." >&2
+MakeMKV is installing..." >&2
 		echo "
-Installing dependencies..." >&2
+		Installing dependencies..." >&2
 		apt-get -y install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev >/dev/null 2>&1
 		wget -O makemkv.html http://www.makemkv.com/download >/dev/null 2>&1
 		makemkv_version=$(cat makemkv.html | grep 'MakeMKV v' | awk -F 'MakeMKV v' '{print $2}' | cut -d ' ' -f 1 | head -n 1)
 		rm makemkv.html
 		echo "
-Downloading MakeMKV..." >&2
+		Downloading MakeMKV..." >&2
 		wget -O makemkv-bin.tar.gz http://www.makemkv.com/download/makemkv-bin-$makemkv_version.tar.gz >/dev/null 2>&1
 		wget -O makemkv-oss.tar.gz http://www.makemkv.com/download/makemkv-oss-$makemkv_version.tar.gz >/dev/null 2>&1
 		echo "
-Opening MakeMKV source..." >&2
+		Opening MakeMKV source..." >&2
 		tar -xf makemkv-bin.tar.gz >/dev/null 2>&1
 		tar -xf makemkv-oss.tar.gz >/dev/null 2>&1
 		rm makemkv-bin.tar.gz >/dev/null 2>&1
 		rm makemkv-oss.tar.gz >/dev/null 2>&1
 		echo "
-Installing MakeMKV OSS..." >&2
+		Installing MakeMKV OSS..." >&2
 		cd makemkv-oss-$makemkv_version
 		./configure >/dev/null 2>&1
 		make >/dev/null 2>&1
 		sudo make install >/dev/null 2>&1
 		echo "
-Installing MakeMKV Bin..." >&2
+		Installing MakeMKV Bin..." >&2
 		cd ../makemkv-bin-$makemkv_version
 		make
 		sudo make install >/dev/null 2>&1
 		echo "
-		MakeMKV has been installed successfully.
+MakeMKV has been installed successfully.
 		" >&2
 		fi
 	;;
 	avahi_daemon)
 		if [[ $avahi ]]; then
+		echo "
+Bonjour Service is installing..." >&2
 		apt-get -y install avahi-daemon >/dev/null 2>&1
 		apt-get -f install >/dev/null 2>&1
+		echo "
+Bonjour Service has been installed successfully.
+		" >&2
 		fi
 	;;
 	ssh_daemon)
 		if [[ $openssh ]]; then
 		sudo systemsetup -setremotelogin on >/dev/null 2>&1
 		echo "
-		Remote Login (ssh) has been switched on.
+Remote Login (ssh) has been switched on.
 		" >&2
 		else
 		sudo systemsetup -setremotelogin off >/dev/null 2>&1
 		echo "
-		Remote Login (ssh) has been switched off.
+Remote Login (ssh) has been switched off.
 		" >&2
 		fi
 	;;
@@ -441,7 +453,6 @@ symlinks
 # Installer for each OS
 if [[ "$OS" == "Mac" ]]; then
 	installer_script homebrew
-	installer_script wget
 	installer_script makemkv_mac
 	installer_script mkvtoolnix_mac
 	installer_script handbrakecli_mac

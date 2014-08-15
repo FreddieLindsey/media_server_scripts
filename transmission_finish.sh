bb27c0f26ae8f80a9d046d9466e56adf31c6f9bd
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2014-08-15 16:07:25 +0100

    Made revision safe (no development)

--- transmission_finish.sh
+++
@@ -1,3 +0,0 @@
-Finds largest file
-
-echo "/""$(find /Users/Freddie/Downloads/Incomplete/James\ Bond\ Die\ Another\ Day\ \(2002\)avchd\ 1080p\(EN\ NL\)\ B-Sam -type f -ls | grep "$(find /Users/Freddie/Downloads/Incomplete/James\ Bond\ Die\ Another\ Day\ \(2002\)avchd\ 1080p\(EN\ NL\)\ B-Sam -type f -ls | awk -F $' ' '{print $7}' | sort -n | tail -n 1)" | cut -d '/' -f 2-)"
